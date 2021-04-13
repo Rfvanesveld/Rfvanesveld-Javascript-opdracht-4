922e2f41a76eec84d7562165e73ba3f5b9f926fc
Rfvanesveld <75245865+Rfvanesveld@users.noreply.github.com>
2021-04-13 14:16:12 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,33 @@
+const leeftijd = 22;
+if (leeftijd > 18) { console.log("Mag naar binnen "); }
+else { console.log("Is te jong"); }
+
+if (leeftijd > 18 && leeftijd < 25) { console.log("Je krijgt 50% discount "); }
+
+else { console.log("Haha gemiste kans, geen korting"); }
+
+const naam = "Sarah";
+
+if (naam == "Bram" || "Sarah") { console.log("Hoppa, 1e biertje gratis") }
+
+const isEenVrouw = true;
+
+if (isEenVrouw) { console.log("Ladiesnight yeah!!!"); }
+
+else { console.log("No men allowed!!"); }
+
+const StatusChauffeur = "Bob";
+
+if (StatusChauffeur == "Bob") { console.log("Dit is de BOB !!"); }
+
+else { console.log("No Driving tonight"); }
+
+const totaalBedrag = 102;
+
+if (totaalBedrag > 25 && totaalBedrag < 50) { console.log("YEAH, Gratis portie bitterballen!!"); }
+
+else if (totaalBedrag > 50 && totaalBedrag < 100) { console.log("HOPPA, Gratis portie nacho's!!!"); }
+
+else if (totaalBedrag > 100) { console.log("HABAHABAHABA Gratis Champagne!!!"); }
+
+else { console.log("NO FREEBEEEEEESS!"); }
